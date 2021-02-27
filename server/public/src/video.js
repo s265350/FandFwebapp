cb477243104bf7c5c270dc435ba3cc5a98b3ddb7
francescoalagna <s265350@studenti.polito.it>
2021-02-27 14:54:54 +0100

    okay?

--- server/public/src/video.js
+++ server/public/src/video.js
@@ -4,6 +4,15 @@
 
 import * as Api from './api.js';
 
+Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+]).then(getStream)
+
+const video = document.getElementById('video')
+
 async function setup(){
     // the user is free to choose any video input attached to the device
     // mediaDevices support check
@@ -17,8 +26,8 @@ async function setup(){
         .then(getStream)
         .catch((error) => {console.error('EnumerateDevices error: ', error)});
     document.getElementById('videoSelector').addEventListener('change', getStream);
-    document.getElementById('video').addEventListener('mousedown', () => {document.getElementById('video').style.opacity = 0.3;});
-    document.getElementById('video').addEventListener('mouseup', () => {document.getElementById('video').style.opacity = 1;});
+    video.addEventListener('mousedown', () => {video.style.opacity = 0.3;});
+    video.addEventListener('mouseup', () => {video.style.opacity = 1;});
 }
 
 function getStream(){
@@ -27,12 +36,84 @@ function getStream(){
         navigator.mediaDevices.getUserMedia({video: {deviceId: { exact: document.getElementById('videoSelector').value },}})
             .then((stream) => {
                 window.stream = stream; // make stream available to console
-                document.getElementById('video').srcObject = stream;})
+                video.srcObject = stream;})
             .catch((error) => {console.error('Video stream error: ', error)});
 }
 
+//individua volto e mostra i Landmarks, se lo porto in getStream riquadri multipli!!
+video.addEventListener('play', async () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    //carico descriptor da immagini etichettate
+    const labeledFaceDescriptors = await loadLabeledImages()
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+    const labeledStrangersDescriptors = await loadLabeledStrangers()
+    const faceMatcherStrangers = new faceapi.FaceMatcher(labeledStrangersDescriptors, 0.6)
+
+    setInterval(async () => { //individuo volto e faccio matching
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors()
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+        const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+        results.forEach((result, i) => { //disegno riquadro e nome attorno al volto
+            if (result.toString().split(' ')[0] == 'unknown') {
+                setInterval(async () => { //individuo volto e faccio matching
+                    const detectionsStranger = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceDescriptors()
+                    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+                    const resizedDetectionsStranger = faceapi.resizeResults(detectionsStranger, displaySize)
+                    const resultsStranger = resizedDetectionsStranger.map(e => faceMatcherStrangers.findBestMatch(e.descriptor))
+                    resultsStranger.forEach((resultS, j) => {
+                        if (resultS.toString().split(' ')[0] == 'unknown') {takeScreenshot()}
+                        const boxS = resizedDetectionsStranger[j].detection.box
+                        const drawBox = new faceapi.draw.DrawBox(boxS, { label: resultS.toString().split(' ')[0] })
+                        drawBox.draw(canvas)
+                    })
+                }, 100)
+                /*const box = resizedDetections[i].detection.box
+                const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString().split(' ')[0] })
+                drawBox.draw(canvas)*/
+            } else {
+                const box = resizedDetections[i].detection.box
+                const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString().split(' ')[0] })
+                drawBox.draw(canvas)
+            }
+        })
+    }, 100)
+})
+
+async function loadLabeledImages() {
+    //etichetto foto
+    const profiles = await Api.getAllProfiles()
+    return Promise.all(
+        profiles.map(async profile => {
+            const descriptions = []
+            const img = await faceapi.fetchImage(await Api.getImage(profile.avatar, false))
+            const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+            if (!detections) {throw new Error(`no faces detected for ${profile.firstName}`)}
+            descriptions.push(detections.descriptor)
+            return new faceapi.LabeledFaceDescriptors(profile.firstName, descriptions)
+        })
+    )
+}
+
+
+async function loadLabeledStrangers() { //etichetto foto stranieri
+    const strangers = await Api.getStrangers()
+    return Promise.all(
+        strangers.map(async stranger => {
+            const descriptions = []
+            const img = await faceapi.fetchImage(await Api.getImage(stranger, true))
+            const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+            if (!detections) {throw new Error(`no faces detected for ${stranger}`)}
+            descriptions.push(detections.descriptor)
+            return new faceapi.LabeledFaceDescriptors(stranger, descriptions)
+        })
+    )
+}
+
 async function takeScreenshot(){
-    const video = document.getElementById('video');
     const canvas = document.createElement('canvas');
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
